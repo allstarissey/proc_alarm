43bd901bcf00accaec2233777ff657dbb261205d
allstarissey <ultimateissey@yahoo.com>
2024-03-27 14:39:40 -0500

    Add dependencies & lay out main fn structure

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,24 @@
-fn main() {}
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, help = "Open command-line interface")]
+    cli: bool,
+}
+
+fn main() {
+    if !sysinfo::IS_SUPPORTED_SYSTEM {
+        // Give unsupported system error
+        todo!()
+    }
+
+    let args = Args::parse();
+
+    if args.cli {
+        // Launch CLI app
+        todo!()
+    }
+
+    // GUI app
+}
