d06b058e9067919dd83e8cd8fa661a5dc4c5df61
allstarissey <ultimateissey@yahoo.com>
2024-03-28 00:08:34 -0500

    Add config file default and specification

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
+use std::path::PathBuf;
 use clap::Parser;
+
+const DEFAULT_CONFIG_FILE_NAME: &str = "proc_alarm.json";
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, help = "Open command-line interface")]
+    #[arg(short='C', long, help = "Open command-line interface")]
     cli: bool,
+
+    #[arg(short, long="config", help = "Use provided config file")]
+    config_path: Option<PathBuf>
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     if !sysinfo::IS_SUPPORTED_SYSTEM {
         // Give unsupported system error
         todo!()
     }
 
     let args = Args::parse();
+    let config_path = match args.config_path {
+        Some(c) => c,
+        None => {
+            let mut path = dirs::config_local_dir().expect("Failed to get default config directory");
+            path.push(DEFAULT_CONFIG_FILE_NAME);
+
+            path
+        }
+    };
 
     if args.cli {
         // Launch CLI app
@@ -21,4 +36,6 @@ fn main() {
     }
 
     // GUI app
+
+    Ok(())
 }
